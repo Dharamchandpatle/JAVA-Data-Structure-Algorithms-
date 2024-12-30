3c0fb896fe24005629528287cc1a335b0ae2dd08
Dharamchand <dharamchandpatle917@gmail.com>
2024-12-31 00:00:47 +0530

    Armstrong

---
+++ Assignment/ArmstrongNumber.java
@@ -0,0 +1,34 @@
+package Assignment;
+
+public class ArmstrongNumber {
+    public static void main(String aaa[])
+    {
+
+    //    boolean ans = isArmstrong(153);
+    //    System.out.println(ans);
+
+    // for 3 digit get
+    for( int i = 100 ; i< 1000; i++)
+    {
+        if(isArmstrong(i)){
+        System.out.print(i + " ");
+
+        }
+    }
+    }
+
+    public static boolean isArmstrong(int n)
+    {
+        int original = n ;
+         int sum = 0 ;
+         while (n>0){
+
+            int rem = n%10 ;
+            n = n/10;
+            sum = sum + rem*rem*rem ;
+
+         }
+         return sum == original ;
+    }
+    
+}
