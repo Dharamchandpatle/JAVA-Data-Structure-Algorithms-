cdd10d22eac9b799142835e74ccfec1a45f03b3a
Dharamchand <dharamchandpatle917@gmail.com>
2025-01-02 23:22:22 +0530

    update

---
+++ ArraysAndArrayLists/Array.java
@@ -0,0 +1,17 @@
+package ArraysAndArrayLists;
+
+public class Array {
+    public static void main (String aa[])
+    {
+        int []arr = new int[5];
+        arr [0]=10;
+        arr [1]=110;
+        arr [2]=102;
+        arr [3]=103;
+        arr [4]=104;
+
+        System.out.println(arr[0]);
+
+    }
+    
+}
