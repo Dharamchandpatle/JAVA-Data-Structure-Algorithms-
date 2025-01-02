c7c284b4c2ae2ecfc783bdb3d4dea6a258143a96
Dharamchand <dharamchandpatle917@gmail.com>
2025-01-02 23:44:55 +0530

    update

--- ArraysAndArrayLists/Array.java
+++ ArraysAndArrayLists/Array.java
@@ -1,8 +1,15 @@
 package ArraysAndArrayLists;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Array {
     public static void main (String aa[])
     {
+        Scanner in = new Scanner(System.in);
+
+        // Array of primitives 
+
         int []arr = new int[5];
         arr [0]=10;
         arr [1]=110;
@@ -10,7 +17,29 @@ public class Array {
         arr [3]=103;
         arr [4]=104;
 
-        System.out.println(arr[0]);
+        System.out.print(arr[0]);
+
+        // For loop 
+        
+        for (int i = 0 ; i<arr.length ; i++){
+            arr[i]= in.nextInt();
+        }
+        for (int i = 0 ; i<arr.length ; i++){
+          System.out.print(arr[i]+" ");
+        }
+
+        // For each 
+        for(int num : arr){
+          System.out.print(num + " ");
+
+        }
+
+        //Best format printing the array 
+        System.out.print(Arrays.toString(arr));
+
+
+    // Array of objects 
+    
 
     }
     
---
+++ ArraysAndArrayLists/ArrayOfObjects.java
@@ -0,0 +1,28 @@
+package ArraysAndArrayLists;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ArrayOfObjects {
+    public static void main(String aa[])    
+    {
+        Scanner in = new Scanner(System.in);
+        // String str = in.nextLine();
+
+        String []str = new String[4];
+
+        for (int i = 0 ; i<str.length ; i++){
+            str[i]=in.nextLine();
+        }
+       
+            System.out.println(Arrays.toString(str));
+        
+
+        // for(String strr : str){
+        //     // System.out.print(strr+" ");
+        //     System.out.print(Arrays.toString(strr));
+        // }
+
+
+    }
+}
