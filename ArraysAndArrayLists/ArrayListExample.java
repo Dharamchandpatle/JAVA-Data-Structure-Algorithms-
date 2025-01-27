eadb34fdb136e647110a4cb15d425554f6150f1d
Dharamchand <dharamchandpatle917@gmail.com>
2025-01-27 22:30:11 +0530

    update

--- ArraysAndArrayLists/ArrayListExample.java
+++ ArraysAndArrayLists/ArrayListExample.java
@@ -4,35 +4,27 @@ import java.util.Scanner;
 import  java.util.ArrayList ;
 
 public class ArrayListExample {
-    public static void main (String aa[]){
-        Scanner in =  new Scanner ( System.in);
+   public static void main (String aa[]){
+    Scanner in = new Scanner(System.in);
 
-        ArrayList <Integer> list = new ArrayList<>(5);
-        // list.add(10);
-        // list.add(130);
-        // list.add(1033);
-        // list.add(4410);
-        // list.add(14044);
-        // list.add(10444342);
+    ArrayList<Integer> list = new ArrayList<>();
+    list.add(1);
+    list.add(1213);
+    list.add(1234);
+    list.add(1234);
+    list.add(1266);
+    list.add(123334);
+    System.out.println(list);
 
-        // System.out.println(list);
-
-        // list.remove(2);
-        // System.out.println(list);
-
-        // System.out.println(list.contains(10));
-
-        // Take input 
-        for ( int i = 0 ; i<5 ; i++){
-            list.add(in.nextInt());
-        }
-
-        //Output 
-        for( int i =0 ; i < 5 ; i++){
-            System.out.println(list.get(i));
-        }
-        System.out.println(list);
-
+    // I am taking input from user 
+    for (int i = 0 ; i < 5 ; i++){
+        list.add(in.nextInt());
     }
+    //Output 
+    for(int i = 0 ; i < 5 ; i ++){
+        System.out.println(list.get(i));
+    }
+    System.out.println(list);
+  }
     
 }
