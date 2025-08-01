f3571610335e2011568386b31f105071d9811d72
Dharamchand <dharamchandpatle917@gmail.com>
2025-08-01 12:46:08 +0530

    Recursion

--- Recursion/FindAllIndex.java
+++ Recursion/FindAllIndex.java
@@ -3,6 +3,7 @@ package Recursion;
 import java.util.Arrays;
 
 public class FindAllIndex {
+    // Iterative method 
     static int[] findIndex(int[] arr, int target) {
         int[] ans = new int[arr.length];
         int k = 0;
@@ -14,11 +15,29 @@ public class FindAllIndex {
        return Arrays.copyOfRange(ans, 0, k);
 
     }
+
+
+    // Recursion Method 
+    static int findAllIndices(int []arr , int target , int n , int idx){
+        // Base case 
+        if( idx >= n){
+            return 0;
+        }
+        // self work 
+        if(arr[idx] == target ){
+            System.out.println(idx);
+        }
+        // Recursive work 
+       return findAllIndices(arr, target, n, idx+1);
+    }
     public static void main(String[] args) {
         int [] arr = {8,3,8,8,4};
         int target = 8 ;
-        int []ans = findIndex(arr, target); 
-        System.out.println(Arrays.toString(ans));
+        int n = arr.length;
+        // int []ans = findIndex(arr, target); 
+        // System.out.println(Arrays.toString(ans));
+
+        System.out.println(findAllIndices(arr, target, n, 0));
     }
 
 }
